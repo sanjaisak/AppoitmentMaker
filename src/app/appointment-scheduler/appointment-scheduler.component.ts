9494f13931e426d8f941f38d008a42126be4f527
Sanjai Sakthivel <sanjaisakthivel2000@gamil.com>
2023-10-23 15:26:31 +0530

    feat(ui): added appointment scheduler

--- src/app/appointment-scheduler/appointment-scheduler.component.ts
+++ src/app/appointment-scheduler/appointment-scheduler.component.ts
@@ -1,8 +1,21 @@
 import { Component } from "@angular/core";
+import { availableDoctorDetails } from "../manage-doctor/doctor-details";
+import { doctorAvailabilityType } from "src/types/enums";
 
 @Component({
   selector: "app-appointment-scheduler",
   templateUrl: "appointment-scheduler.component.html",
   styleUrls: ["appointment-scheduler.component.scss"],
 })
-export class AppointmentSchedularComponent {}
+export class AppointmentSchedularComponent {
+  /**
+   * initialization of doctor details from doctor-details file
+   */
+  public getAvailableDoctor() {
+    const doctors = availableDoctorDetails.filter(
+      (doctor) => doctor.availability === doctorAvailabilityType.available
+    );
+
+    return doctors;
+  }
+}
--- src/app/appointment-scheduler/appointment-scheduler.module.ts
+++ src/app/appointment-scheduler/appointment-scheduler.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { IonicModule } from "@ionic/angular";
+import { BrowserModule } from "@angular/platform-browser";
 import { AppointmentSchedularComponent } from "./appointment-scheduler.component";
 
 @NgModule({
-  imports: [IonicModule],
+  imports: [IonicModule, BrowserModule],
   declarations: [AppointmentSchedularComponent],
 })
 export class AppointmentSchedulerModule {}
