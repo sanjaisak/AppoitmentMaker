65fb4b5ec92cc9f66df28d48d6abdfc94b651a84
Sanjai Sakthivel <sanjaisakthivel2000@gamil.com>
2023-10-23 10:37:58 +0530

    refactor(ui): cleaned folder structure for edit doctor dialog
    Just changed the folder structure and reanamed to make dialog component more meaning full

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,23 +1,30 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
-import { HomePageRoutingModule } from './home-routing.module';
-import { SideNavBarComponent } from '../side-nav-bar/side-nav-bar.component';
-import { ContainerComponent } from '../container/container.component';
-import { ManageDoctorComponent } from '../manage-doctor/manage-doctor.component';
-import { NavigateComponent } from '../navigate-component.directive';
-import { EditDoctorDetailsModalComponent } from '../edit-doctorDetails-modal.component/edit-doctorDetails-modal.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { IonicModule } from "@ionic/angular";
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HomePage } from "./home.page";
+import { HomePageRoutingModule } from "./home-routing.module";
+import { SideNavBarComponent } from "../side-nav-bar/side-nav-bar.component";
+import { ContainerComponent } from "../container/container.component";
+import { ManageDoctorComponent } from "../manage-doctor/manage-doctor.component";
+import { NavigateComponent } from "../navigate-component.directive";
+import { EditDoctorDetailsComponent } from "../manage-doctor/edit-doctor-details/edit-doctor-details.component";
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
     HomePageRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
-  declarations: [HomePage,SideNavBarComponent,ContainerComponent,ManageDoctorComponent,NavigateComponent,EditDoctorDetailsModalComponent]
+  declarations: [
+    HomePage,
+    SideNavBarComponent,
+    ContainerComponent,
+    ManageDoctorComponent,
+    NavigateComponent,
+    EditDoctorDetailsComponent,
+  ],
 })
 export class HomePageModule {}
--- src/app/manage-doctor/manage-doctor.component.ts
+++ src/app/manage-doctor/manage-doctor.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { Validators, FormBuilder } from '@angular/forms';
-import { doctorAvailabilityType, doctorGenderType } from '../../types/enums';
-import { doctorDetails } from '../../types/interfaces';
-import { availableDoctorDetails } from './doctor-details';
-import { EditDoctorDetailsModalComponent } from '../edit-doctorDetails-modal.component/edit-doctorDetails-modal.component';
+import { Component, OnInit } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { Validators, FormBuilder } from "@angular/forms";
+import { doctorAvailabilityType, doctorGenderType } from "../../types/enums";
+import { doctorDetails } from "../../types/interfaces";
+import { availableDoctorDetails } from "./doctor-details";
+import { EditDoctorDetailsComponent } from "./edit-doctor-details/edit-doctor-details.component";
 
 @Component({
-  selector: 'app-manage-doctor',
-  templateUrl: './manage-doctor.component.html',
-  styleUrls: ['./manage-doctor.component.scss'],
+  selector: "app-manage-doctor",
+  templateUrl: "./manage-doctor.component.html",
+  styleUrls: ["./manage-doctor.component.scss"],
 })
-
 export class ManageDoctorComponent {
-
   /** initilization of doctorAvailabilityType from doctorAvailabilityType enum which contains the weather the doctor is available or unavailable or removed */
   public doctorAvailabilityType = doctorAvailabilityType;
 
@@ -29,33 +27,44 @@ export class ManageDoctorComponent {
   /** initilization of doctor details from doctor-details file */
   public availableDoctorDetails = availableDoctorDetails;
 
-  constructor(private formBuilder: FormBuilder, private modalCtrl: ModalController) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private modalCtrl: ModalController
+  ) {
     /** defining the valiadation and initilizatoin of form builder */
     this.doctorInfo = this.formBuilder.group({
-      doctorName: ['', [Validators.required, Validators.maxLength(20)]],
-      speciality: ['', [Validators.required]],
-      dob: ['', [Validators.required]],
-      phoneNumber: ['', [Validators.required, Validators.pattern("^[0-9]{10}$")]],
-      gender: ['', [Validators.required]]
-    })
+      doctorName: ["", [Validators.required, Validators.maxLength(20)]],
+      speciality: ["", [Validators.required]],
+      dob: ["", [Validators.required]],
+      phoneNumber: [
+        "",
+        [Validators.required, Validators.pattern("^[0-9]{10}$")],
+      ],
+      gender: ["", [Validators.required]],
+    });
   }
 
   /**
    * used to check the required condition in validation
    * @param input accepts the input formcontrol
-   * @returns boolean 
+   * @returns boolean
    */
   public required(input: String): boolean {
-    return this.doctorInfo.get(input).errors?.required &&
+    return (
+      this.doctorInfo.get(input).errors?.required &&
       this.doctorInfo.get(input).touched
+    );
   }
 
   /**
-  * used to change the doctor status
-  * @param doctorId accepts the index of the doctor in availableDoctorDetails
-  * @param changeStatus accepts to which the doctor's status to be changed
-  */
-  public changeDoctorStatus(doctorId: number, changeStatus: doctorAvailabilityType) {
+   * used to change the doctor status
+   * @param doctorId accepts the index of the doctor in availableDoctorDetails
+   * @param changeStatus accepts to which the doctor's status to be changed
+   */
+  public changeDoctorStatus(
+    doctorId: number,
+    changeStatus: doctorAvailabilityType
+  ) {
     this.availableDoctorDetails[doctorId].availability = changeStatus;
   }
 
@@ -64,14 +73,15 @@ export class ManageDoctorComponent {
    */
   public storeDoctorInfo() {
     this.details = {
-      photo: "https://img.freepik.com/free-photo/woman-doctor-wearing-lab-coat-with-stethoscope-isolated_1303-29791.jpg",
-      name: this.doctorInfo.get('doctorName').value,
+      photo:
+        "https://img.freepik.com/free-photo/woman-doctor-wearing-lab-coat-with-stethoscope-isolated_1303-29791.jpg",
+      name: this.doctorInfo.get("doctorName").value,
       availability: doctorAvailabilityType.available,
-      dob: new Date(this.doctorInfo.get('dob').value),
-      phoneNumber: Number(this.doctorInfo.get('phoneNumber').value),
-      speciality: this.doctorInfo.get('speciality').value,
-      gender: this.doctorInfo.get('gender').value
-    }
+      dob: new Date(this.doctorInfo.get("dob").value),
+      phoneNumber: Number(this.doctorInfo.get("phoneNumber").value),
+      speciality: this.doctorInfo.get("speciality").value,
+      gender: this.doctorInfo.get("gender").value,
+    };
     this.availableDoctorDetails.push(this.details);
     this.doctorInfo.reset();
   }
@@ -81,13 +91,12 @@ export class ManageDoctorComponent {
    * @param selectedDoctorDetails accepts the selected doctor details object to be edited
    */
   public async editModalOpen(selectedDoctorDetails: doctorDetails) {
-
     /** used to crteate the edit modal and store the reference */
     const editModal = await this.modalCtrl.create({
-      component: EditDoctorDetailsModalComponent,
+      component: EditDoctorDetailsComponent,
       componentProps: {
-        selectedDoctorDetails: selectedDoctorDetails
-      }
+        selectedDoctorDetails: selectedDoctorDetails,
+      },
     });
 
     /** presents or displays the model to the view */
@@ -95,12 +104,12 @@ export class ManageDoctorComponent {
 
     const { data, role } = await editModal.onWillDismiss();
 
-    if (role === 'save') {
-      selectedDoctorDetails.name = data.get('doctorName').value;
-      selectedDoctorDetails.speciality = data.get('speciality').value;
-      selectedDoctorDetails.dob = new Date(data.get('dob').value);
-      selectedDoctorDetails.gender = data.get('gender').value;
-      selectedDoctorDetails.phoneNumber = data.get('phoneNumber').value;
+    if (role === "save") {
+      selectedDoctorDetails.name = data.get("doctorName").value;
+      selectedDoctorDetails.speciality = data.get("speciality").value;
+      selectedDoctorDetails.dob = new Date(data.get("dob").value);
+      selectedDoctorDetails.gender = data.get("gender").value;
+      selectedDoctorDetails.phoneNumber = data.get("phoneNumber").value;
     }
   }
 }
